37d8c37eb1ea1a44f54efc14343b4237f7cebe44
Alexander Kurz <alex.kurz95@gmail.com>
2026-06-10 13:35:43 +0200

    Encode and decode face image

--- interpolate.py
+++ interpolate.py
@@ -3,27 +3,48 @@
 from pprint import pprint
 
 import torch
+import torchvision.transforms.functional as tfmF
 from diffusers import DDIMScheduler, StableDiffusionPipeline
 from PIL import Image
+
+from src.gaussian_diffusion import invert, sample
 
 # %%
 # Load pipeline
 device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
 pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5").to(device)
-print(pipe.device)
-pprint(dict(pipe.config), sort_dicts=False)
 
-# %%
-# Test pipeline
 # Set up a DDIM scheduler
 pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
 assert isinstance(pipe.scheduler, DDIMScheduler)
 
-# Sample an image to make sure it is all working
-prompt = "Beautiful DSLR Photograph of a penguin on the beach, golden hour"
-negative_prompt = "blurry, ugly, stock photo"
-img: Image.Image = pipe(prompt, negative_prompt=negative_prompt).images[0]
-# print(img)  # Returned image is of size 512x512 px
-img.resize((256, 256))  # Resize for convenient viewing
+# Print pipe setup
+print(pipe.device)
+pprint(dict(pipe.config), sort_dicts=False)
+
+# %%
+# Encode and decode image
+input_image = Image.open("data/1_a.png").resize((512, 512))
+input_image_prompt = "a photo of a person"
+with torch.no_grad():
+    latent = pipe.vae.encode(tfmF.to_tensor(input_image).unsqueeze(0).to(device) * 2 - 1)
+latent_vec = 0.18215 * latent.latent_dist.sample()
+print("Encoding image:")
+inverted_latents = invert(
+    pipe,
+    latent_vec,
+    input_image_prompt,
+    num_inference_steps=50,
+)
+print(inverted_latents.shape)
+print("Decoding image:")
+start_step = 20
+sample(
+    pipe,
+    input_image_prompt,
+    start_latents=inverted_latents[-(start_step + 1)][None],
+    start_step=start_step,
+    num_inference_steps=50,
+)[0]
 
 # %%
